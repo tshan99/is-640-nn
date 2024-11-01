f806697fad855ec0e67ae733ba6ea3dd4b616fd4
Shantanu Tumbde <shantanu.tumbde01@student.csulb.edu>
2024-11-01 14:46:51 -0700

    third commit

--- engine.py
+++ engine.py
@@ -4,7 +4,7 @@ class Value:
     def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0
-       
+       # internal variables used for autograd graph construction
         self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op # the op that produced this node, for graphviz / debugging / etc
@@ -55,7 +55,7 @@ class Value:
     
     def backward(self):
 
-        # topological order of all of the children in the graph
+       
         topo = []
         visited = set()
         def build_topo(v):
