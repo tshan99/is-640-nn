4d3808324cab1e87824b454de1df521e59068e6b
tshan99 <82194019+tshan99@users.noreply.github.com>
2024-11-08 20:24:13 -0800

    Update main.py "fourth commit"

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ xs = [[2.0, 3.0, -1.0],
       [0.5, 1.0, 1.0], 
       [1.0, 1.0, -1.0]]  # Input data
 
-ys = [0.0, 1.0, 1.0, 0.0]  
+ys = [0.0, 1.0, 1.0, 0.0]  # Target outputs
 
 # Initialize the MLP with input and output sizes
 n = MLP(3, [4, 4, 1])
